774d1a7a3002a528a2ead7f2f1e55f4d949d7d4a
Nitikorn-T <ichromex@hotmail.com>
2021-11-29 12:38:32 +0700

    Send notify with lat lon

--- pySchedule.py
+++ pySchedule.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from firebase_admin import db
 from firebase_admin import credentials
 import firebase_admin
@@ -10,37 +11,18 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# firebaseConfig = {
-#   apiKey: "AIzaSyArYzvqFew8nxMdyN4RvDfZ4Pe_lFY9Y4w",
-#   authDomain: "traffic-anomaly-detection.firebaseapp.com",
-#   databaseURL: "https://traffic-anomaly-detection-default-rtdb.asia-southeast1.firebasedatabase.app",
-#   projectId: "traffic-anomaly-detection",
-#   storageBucket: "traffic-anomaly-detection.appspot.com",
-#   messagingSenderId: "1010599694991",
-#   appId: "1:1010599694991:web:d2c3013e28f7ab1b6716fa",
-#   measurementId: "G-VR7QWSMPR8"
-# };
-
 PATH = os.getenv('PATH_WEB')
 LINE_URL = os.getenv('LINE_URL')
 LINE_TOKEN = os.getenv('LINE_TOKEN')
+DB_URL = os.getenv('DB_URL')
 
 cred = credentials.Certificate("firebase-sdk.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://traffic-anomaly-detection-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    'databaseURL': DB_URL
 })
 
 
 df_km127 = pd.read_csv("./dataset/latlon_km127.csv")
-
-ref = db.reference('Accident')
-data = {
-    'coor': {
-        'lat': 113.001,
-        'lon': 114.101
-    }
-}
-ref.push(data)
 
 
 def csv_file():
@@ -51,10 +33,12 @@ def csv_file():
                   'avg_traveltime', 'max_traveltime'], axis=1)
     df_traffic = filter_traffic(df)
     df_traffic_wlatlon = map_traffic_with_latlon(df_traffic)
-    map_traffic_with_latlon(df_traffic)
     print(df_traffic_wlatlon)
-    line_notify()
-
+    acclat = str(df_traffic_wlatlon.loc[0, 'lat'])
+    acclon = str(df_traffic_wlatlon.loc[0, 'lon'])
+    accdate = str(df_traffic_wlatlon.loc[0, 'datetime'])
+    post_db(accdate, acclat, acclon)
+    line_notify(acclat, acclon, accdate)
 
 def filter_traffic(df):
     return df[(df['road_number'] == 1) | (df['road_number'] == 2) | (df['road_number'] == 7)]
@@ -66,25 +50,32 @@ def map_traffic_with_latlon(df):
     df['lon'] = df.apply(lambda row: df_km127[(df_km127['rd'] == row['road_number']) & (df_km127['km'] == row['km'])]['lon'].values[0]
                          if len(df_km127[(df_km127['rd'] == row['road_number']) & (df_km127['km'] == row['km'])]['lon'].values) > 0 else 0, axis=1)
     return df
-    # return df
 
-
-def line_notify():
+def line_notify(lat, lon, date):
     headers = {
         'content-type':
             'application/x-www-form-urlencoded',
             'Authorization': 'Bearer ' + LINE_TOKEN
     }
     # msg = input("Enter your name:")
-    msg = "Send every 2 minutes"
+    msg = date + " Accident at " + "Latitude : "+lat + ", Longitude : "+lon
     r = requests.post(LINE_URL, headers=headers, data={'message': msg})
     print(r.text)
 
-# print(df_km127[(df_km127['rd']==1) & (df_km127['km']==815)]['lat'].item())
 
+def post_db(date, lat, lon):
+    ref = db.reference('Accident')
+    data = {
+        'datetime': date,
+        'coor': {
+            'lat': lat,
+            'lon': lon
+        }
+    }
+    ref.push(data)
 
 # schedule.every(2).seconds.do(csv_file)
-schedule.every(2).minutes.do(csv_file)
+schedule.every(1).minutes.do(csv_file)
 # schedule.every(2).minutes.do(line_notify)
 # schedule.every().hour.do(csv_file)
 # schedule.every().day.at("10:30").do(csv_file)
@@ -92,8 +83,6 @@ schedule.every(2).minutes.do(csv_file)
 # schedule.every().monday.do(csv_file)
 # schedule.every().wednesday.at("13:15").do(csv_file)
 # schedule.every().minute.at(":17").do(csv_file)
-# csv_file()
-# line_notify()
 while True:
     schedule.run_pending()
     time.sleep(1)
