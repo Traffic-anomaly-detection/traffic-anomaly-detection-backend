21368f6926f58f3841cfcd4b5043b0ca978aa4d3
Nitikorn-T <ichromex@hotmail.com>
2022-01-19 17:43:45 +0700

    - use lib

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
