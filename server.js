eedc6df3bb762bdf5934f171de45b085b7d37ca4
Nitikorn-T <ichromex@hotmail.com>
2022-01-27 00:07:36 +0700

    - get accident, - get accident by params, - create accident data

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./configs/db");
+const apiRoutes = require("./routes/index");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const port = process.env.PORT;
@@ -13,6 +15,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api", apiRoutes);
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
